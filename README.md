0249fff3f72d25c2dbc3a0266b902cecc934f316
Christopher Wells <chrisw@wellswebtech.co.za>
2025-01-31 10:42:17 +0200

    Initial Commit
    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# l11_reverb_example
+ Laravel 11 Simple Reverb Project
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Use ProfileController
+use App\Http\Controllers\ProfileController;
+// Add Broadcast
+use Illuminate\Support\Facades\Broadcast;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/broadcast', function () {
+    Broadcast::on('global')
+        ->as('Message')
+        ->with([
+            'body' => str()->random(10)
+        ])
+        ->sendNow();
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
